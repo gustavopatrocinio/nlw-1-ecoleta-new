b5eee0dfe400ba14a19afcc98831bf4bbffc49de
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2021-02-15 21:57:54 -0300

    Identifying application entities

---
+++ server/src/database/migrations/00_create_points.ts
@@ -0,0 +1,20 @@
+import knex from 'knex';
+
+export async function up(knex: knex) {
+    return knex.schema.createTable('points', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('latitude').notNullable();
+        table.string('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+
+    });
+}
+
+export async function down(kenx: knex) {
+    return kenx.schema.dropTable('point');
+}
