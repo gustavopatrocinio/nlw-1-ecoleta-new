12d04bef66427829d67c2a4d807eff44766fa5f7
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2021-02-17 21:57:07 -0300

    Building the HTML of the first page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <h1>Conteúdo da aplicação</h1>
-    </div>
-        
+    <Home />
   );
 }
 
